f93fef33d40d6ed2019ecfb810371c3f8c08286f
William Ashfield <wilashfie@gmail.com>
2024-03-29 11:43:21 -0600

    create Tarr obj class for import

---
+++ analysis/load_save.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.io import readsav
+
+
+class Tarr:
+    def __init__(self, file):
+        save_d = readsav(file,verbose=False,python_dict=False)
+        #recarray = save_d["tarr"]
+        #self.recarray = recarray
+        self._recarray = save_d["tarr"]
+        self.variables = self._recarray.dtype.names
+
+    def __getattr__(self, field_name):
+        return np.stack(self._recarray[field_name])
+    
+    def print_var(self):
+        print(self.variables)
